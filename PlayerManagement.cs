69e0dd8204cf9e3f5098a38a43fc770ba2b6d5bd
Moona Partanen <moonapartanen@outlook.com>
2017-10-08 19:44:42 +0300

    Bugit korjattu, kaikki valmiina

---
+++ PlayerManagement.cs
@@ -0,0 +1,187 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hockey_Database
+{
+    public partial class PlayerManagement : Form
+    {
+        string playerManagementQuery = "SELECT players.ID, players.name, players.dateOfBirth, players.number, teams.name, positions.position " +
+                                       "FROM players " +
+                                       "INNER JOIN teams ON players.team_ID = teams.ID " +
+                                       "INNER JOIN positions ON players.position_ID = positions.ID; ";
+
+        public string teamsQuery = "SELECT DISTINCT ID, name " +
+                                    "FROM teams";
+
+        // LUODAAN DB-OLIO
+        dbConnect db = new dbConnect();
+        int id, positionID;
+
+        public PlayerManagement()
+        {
+            InitializeComponent();
+        }
+
+        private int GetPositionId()
+        {
+            int positionID = 0;
+
+            if (rbForward_pm.Checked)
+            {
+                positionID = 1;
+            }
+            else if (rbDefender_pm.Checked)
+            {
+                positionID = 2;
+            }
+            else if (rbGoalie_pm.Checked)
+            {
+                positionID = 3;
+            }
+
+            return positionID;
+        }
+
+        private bool CheckIfEmpty()
+        {
+            bool ret = true;
+
+            if (rbDefender_pm.Checked || rbForward_pm.Checked || rbGoalie_pm.Checked)
+            {
+                if (txtName_pm.Text.Length == 0 || txtNumber_pm.Text.Length == 0)
+                {
+                    ret = false;
+                }
+            }
+            else
+            {
+                ret = false;
+            }
+
+            return ret;
+        }
+
+        private void SelectPlayerManagement()   // HAKEE TIEDOT DATAGRIDVIEWIIN
+        {
+            dgPlayerManagement.DataSource = db.Select(playerManagementQuery);
+
+            dgPlayerManagement.Columns[0].HeaderText = "ID:";                    
+            dgPlayerManagement.Columns[1].HeaderText = "Pelaajan nimi:";
+            dgPlayerManagement.Columns[2].HeaderText = "Syntymäaika:";
+            dgPlayerManagement.Columns[3].HeaderText = "Pelinumero:";
+            dgPlayerManagement.Columns[4].HeaderText = "Joukkue:";
+            dgPlayerManagement.Columns[5].HeaderText = "Pelipaikka:";
+        }
+
+        private void PlayerManagement_Load(object sender, EventArgs e)
+        {
+            //db.OpenConnection();   
+            SelectPlayerManagement();
+
+            cmbTeams_pm.SelectedIndex = -1;
+            cmbTeams_pm.DataSource = db.Select(teamsQuery);
+            cmbTeams_pm.DisplayMember = "name";
+            cmbTeams_pm.ValueMember = "ID";
+        }
+
+        private void dgPlayerManagement_SelectionChanged(object sender, EventArgs e)    // KUN ROW SELECTION VAIHTUU
+        {
+            if (dgPlayerManagement.SelectedRows.Count > 0) 
+            {
+                txtName_pm.Text = dgPlayerManagement.SelectedRows[0].Cells[1].Value + string.Empty;
+                dpDateOfBirth_pm.Value = Convert.ToDateTime(dgPlayerManagement.SelectedRows[0].Cells[2].Value);
+                cmbTeams_pm.Text = dgPlayerManagement.SelectedRows[0].Cells[4].Value + string.Empty;
+                txtNumber_pm.Text = dgPlayerManagement.SelectedRows[0].Cells[3].Value + string.Empty;
+
+                switch (dgPlayerManagement.SelectedRows[0].Cells[5].Value + string.Empty)
+                {
+                    case "Hyökkääjä":
+                        rbForward_pm.Checked = true;
+                        positionID = 1;
+                        break;
+                    case "Puolustaja":
+                        rbDefender_pm.Checked = true;
+                        positionID = 2;
+                        break;
+                    case "Maalivahti":
+                        rbGoalie_pm.Checked = true;
+                        positionID = 3;
+                        break;
+                }
+
+                id = Convert.ToInt32(dgPlayerManagement.SelectedRows[0].Cells[0].Value);    // ID TALTEEN POISTOA TAI MUOKKAUSTA VARTEN
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)     // TYHJENNETÄÄN KENTÄT      TODO: TÄHÄN VARMASTI PAREMPI TAPA OLEMASSA
+        {
+            dgPlayerManagement.ClearSelection();       
+            txtName_pm.Text = "";
+            cmbTeams_pm.Text = "";      
+            txtNumber_pm.Text = "";
+            dpDateOfBirth_pm.Text = "";
+            rbForward_pm.Checked = false;
+            rbDefender_pm.Checked = false;
+            rbGoalie_pm.Checked = false;
+            id = 0;
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (dgPlayerManagement.SelectedRows.Count > 0)
+            {
+                string query = "DELETE FROM players WHERE ID = " + dgPlayerManagement.SelectedRows[0].Cells[0].Value + string.Empty;
+                db.Delete(query);
+                MessageBox.Show("Poisto onnistui!");
+                SelectPlayerManagement();
+            }
+            else
+            {
+                MessageBox.Show("Et ole valinnut poistettavaa riviä!");
+            }
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string playerEditQuery = "SELECT * FROM players WHERE ID = " + id + ";";
+            DataTable dt = db.Select(playerEditQuery);
+
+            if(CheckIfEmpty())
+            {
+                if (dt.Rows.Count > 0)
+                {
+
+                    string date = dpDateOfBirth_pm.Value.ToString("yyyy-MM-dd").Replace('.', '-');
+                    string query = "UPDATE players SET name='" + txtName_pm.Text + "', dateOfBirth='" + date + "', number=" + txtNumber_pm.Text + ", team_ID=" + cmbTeams_pm.SelectedValue.ToString() + ", position_ID=" + GetPositionId() + " WHERE ID=" + id;
+                    db.Update(query);
+                    MessageBox.Show("Tietojen päivitys onnistui!");
+                    SelectPlayerManagement();
+
+                }
+                else
+                {
+                    string date = dpDateOfBirth_pm.Value.ToString("yyyy-MM-dd").Replace('.', '-');
+                    string query = "INSERT INTO players (name, dateOfBirth, number, team_ID, position_ID) " +
+                                   "VALUES ('" + txtName_pm.Text + "', '" + date + "', "
+                                   + txtNumber_pm.Text + ", " + cmbTeams_pm.SelectedValue.ToString() + ", "
+                                   + GetPositionId() + ");";
+
+                    db.Insert(query);
+                    MessageBox.Show("Tietojen lisäys onnistui!");
+                    SelectPlayerManagement();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Taytä kentät!");
+            }
+        }
+    }
+}
---
+++ TeamManagement.cs
@@ -0,0 +1,164 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Hockey_Database
+{
+    public partial class TeamManagement : Form
+    {
+        string teamManagementQuery = "SELECT teams.ID, teams.name, stadiums.name, leagues.name, coaches.name " +
+                                     "FROM teams " +
+                                     "INNER JOIN stadiums ON teams.stadium_ID = stadiums.ID " +
+                                     "INNER JOIN leagues ON teams.league_ID = leagues.ID " +
+                                     "INNER JOIN coaches ON teams.coach_ID = coaches.ID ";
+
+        string stadiumsQuery = "SELECT DISTINCT ID, name " +
+                                    "FROM stadiums";
+
+        string leaguesQuery = "SELECT DISTINCT ID, name " +
+                              "FROM leagues";
+
+        string coachesQuery = "SELECT DISTINCT ID, name " +
+                              "FROM coaches";
+
+        dbConnect db = new dbConnect();
+
+        int id;
+
+        public TeamManagement()
+        {
+            InitializeComponent();
+        }
+
+        private bool CheckIfEmpty()
+        {
+            bool ret = true;
+
+            if (txtName_tm.Text.Length == 0 || cmbCoaches_tm.SelectedIndex == -1 || cmbLeagues_tm.SelectedIndex == -1 || cmbStadiums_tm.Text.Length == -1)
+            {
+                ret = false;
+            }
+
+            return ret;
+        }
+
+        private void SelectTeamManagement()
+        {
+            dgTeamManagement.DataSource = db.Select(teamManagementQuery);
+
+            dgTeamManagement.Columns[0].HeaderText = "ID:";
+            dgTeamManagement.Columns[1].HeaderText = "Joukkueen nimi:";
+            dgTeamManagement.Columns[2].HeaderText = "Kotistadion:";
+            dgTeamManagement.Columns[3].HeaderText = "Liiga:";
+            dgTeamManagement.Columns[4].HeaderText = "Päävalmentaja:";
+
+            dgTeamManagement.ClearSelection();  // POISTETAAN SAMALLA DATAGRIDVIEWIN VALINTA
+        }
+
+        private void FillComboboxes()
+        {
+            // HAETAAN COMBOBOXEIHIN TIEDOT
+
+            cmbStadiums_tm.DataSource = db.Select(stadiumsQuery);
+            cmbStadiums_tm.DisplayMember = "name";
+            cmbStadiums_tm.ValueMember = "ID";
+            cmbStadiums_tm.SelectedIndex = -1; 
+
+            cmbLeagues_tm.DataSource = db.Select(leaguesQuery);
+            cmbLeagues_tm.DisplayMember = "name";
+            cmbLeagues_tm.ValueMember = "ID";
+            cmbLeagues_tm.SelectedIndex = -1;
+
+            cmbCoaches_tm.DataSource = db.Select(coachesQuery);
+            cmbCoaches_tm.DisplayMember = "name";
+            cmbCoaches_tm.ValueMember = "ID";
+            cmbCoaches_tm.SelectedIndex = -1;
+        }
+
+        private void TeamManagement_Load(object sender, EventArgs e)
+        {
+            SelectTeamManagement();
+            FillComboboxes();
+        }
+
+        private void dgTeamManagement_SelectionChanged(object sender, EventArgs e)
+        {
+            if (dgTeamManagement.SelectedRows.Count > 0)
+            {
+                txtName_tm.Text = dgTeamManagement.SelectedRows[0].Cells[1].Value + string.Empty;
+                cmbStadiums_tm.Text = dgTeamManagement.SelectedRows[0].Cells[2].Value + string.Empty;
+                cmbLeagues_tm.Text = dgTeamManagement.SelectedRows[0].Cells[3].Value + string.Empty;
+                cmbCoaches_tm.Text = dgTeamManagement.SelectedRows[0].Cells[4].Value + string.Empty;
+
+                id = Convert.ToInt32(dgTeamManagement.SelectedRows[0].Cells[0].Value);    // ID TALTEEN POISTOA TAI MUOKKAUSTA VARTEN
+            }
+        }
+
+        private void btnClear_Click(object sender, EventArgs e)
+        {
+            dgTeamManagement.ClearSelection();
+            txtName_tm.Text = "";
+            cmbStadiums_tm.Text = "";
+            cmbLeagues_tm.Text = "";
+            cmbCoaches_tm.Text = "";
+
+            id = 0;
+        }
+
+        private void btnSave_Click(object sender, EventArgs e)
+        {
+            string teamEditQuery = "SELECT * FROM teams WHERE ID = " + id + ";";
+            DataTable dt = db.Select(teamEditQuery);
+
+            if (CheckIfEmpty())
+            {
+                if (dt.Rows.Count > 0)
+                {
+                    string query = "UPDATE teams SET name='" + txtName_tm.Text + "', stadium_ID=" + cmbStadiums_tm.SelectedValue.ToString() + ", league_ID=" + cmbLeagues_tm.SelectedValue.ToString() + ", coach_ID=" + cmbCoaches_tm.SelectedValue.ToString() + " WHERE ID=" + id;
+                    db.Update(query);
+                    MessageBox.Show("Tietojen päivitys onnistui!");
+                    SelectTeamManagement();
+
+                }
+                else
+                {
+                    string query = "INSERT INTO teams (name, stadium_ID, league_ID, coach_ID) " +
+                                    "VALUES ('" + txtName_tm.Text + "', " + cmbStadiums_tm.SelectedValue.ToString() + ", " + cmbLeagues_tm.SelectedValue.ToString() + ", " + cmbCoaches_tm.SelectedValue.ToString() + ");";
+
+                    db.Insert(query);
+                    MessageBox.Show("Tietojen lisäys onnistui!");
+                    SelectTeamManagement();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Täytä kentät!");
+            }
+         
+        }
+
+        private void btnDel_Click(object sender, EventArgs e)
+        {
+            if (dgTeamManagement.SelectedRows.Count > 0)
+            {
+                if (MessageBox.Show("Jos valitsemassasi joukkueessa on pelaajia, pelaajat poistetaan joukkueen mukana. Haluatko silti poistaa joukkueen?", "Varoitus", MessageBoxButtons.YesNo) == DialogResult.Yes)
+                {
+                    string query = "DELETE FROM teams WHERE ID = " + id;
+                    db.Delete(query);
+                    MessageBox.Show("Poisto onnistui!");
+                    SelectTeamManagement();
+                }
+            }
+            else
+            {
+                MessageBox.Show("Et ole valinnut poistettavaa riviä!");
+            }
+        }
+    }
+}
